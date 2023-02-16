0fb905b647ad98aea03d0eb3a9146133476cbc30
Demilade Folarin <demiladefolarin@gmail.com>
2023-02-16 07:44:19 +0100

    adding my firebaseConfig to gitignore hide

--- src/components/Context/AuthContext.jsx
+++ src/components/Context/AuthContext.jsx
@@ -6,7 +6,7 @@ import {
     onAuthStateChanged, 
     signOut 
 } from "firebase/auth";
-import { auth } from '../../firebase';
+import { auth } from '../../.firebase';
 
 const UserContext = createContext()
 
